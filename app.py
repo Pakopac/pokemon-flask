37aa8a182e40c5559c41e5658e3deecab899c4b7
Lilian.Pacaud <Lilian.Pacaud@sky-real.com>
2020-12-02 16:18:38 +0100

    Add list pokemons page

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ from jinja2 import Template
 import json
 import os
 
+# json importation
+pokemon_file = "./static/assets/pokemon_datas/pokedex.json"
+with open(pokemon_file) as json_file:
+    pokemon_load = json.load(json_file)
+pokemon_dump = json.dumps(pokemon_load)
+pokemon_json = json.loads(pokemon_dump)
+
+type_file = "./static/assets/pokemon_datas/types.json"
+with open(type_file) as json_type_file:
+    file_load = json.load(json_type_file)
+file_dump = json.dumps(file_load)
+type_file_json = json.loads(file_dump)
+
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
@@ -12,9 +25,9 @@ port = int(os.environ.get("PORT", 5000))
 def home():
     return render_template('home.html')
 
-@app.route('/project')
+@app.route('/pokemons')
 def project():
-    return render_template('project.html')
+    return render_template('pokemons.html', pokemons=pokemon_json, types=type_file_json)
 
 @app.route('/contact')
 def contact():
